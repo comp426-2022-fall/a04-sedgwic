b4310be3675876f8edd38281df13ca909ae95671
sedgwic <sedgwic@email.unc.edu>
2022-11-30 14:07:32 -0500

    endpoints

--- server.js
+++ server.js
@@ -20,8 +20,36 @@ app.get('/app/', (req, res) => {
   res.send("200 OK");
 });
 
+// default endpoint
+app.post('/app/roll', (req, res) => {
+  res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
+});
+
+app.get('/app/roll', (req, res) => {
+  res.send(roll(6, 2, 1));
+});
+
+// endpoint for sides
+app.get('/app/roll/:sides/', (req, res) => {
+  res.send(roll(parseInt(req.body.sides),2, 1));
+});
+
+// endpoint for sides and dice
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+  res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), 1));
+});
+
+// endpoint for sides, dice, and rolls
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+  res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
+});
+
+// endpoint for 404 error
 app.get('/app/*', (req, res) => {
   res.send("404 NOT FOUND");
 });
 
-app.listen(port);
+// start her up
+app.listen(port, () => {
+  console.log('Server port ${port}\n');
+});
